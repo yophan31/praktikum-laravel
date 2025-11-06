bf8dbf4d0db0886801ad88b1a90f6de0d0e7c8c1
D.Yophanci P. Sihombing <207501472+yophan31@users.noreply.github.com>
2025-11-06 08:59:42 +0700

    i

--- resources/views/livewire/home-livewire.blade.php
+++ resources/views/livewire/home-livewire.blade.php
@@ -2,7 +2,7 @@
     <div class="card">
         <div class="card-header d-flex">
             <div class="flex-fill">
-                <h3>Hay, {{ $auth->name }}</h3>
+                <h3 class="fw-bold">Hay, {{ $auth->name }}</h3>
             </div>
             <div>
                 <a href="{{ route('auth.logout') }}" class="btn btn-warning">Keluar</a>
